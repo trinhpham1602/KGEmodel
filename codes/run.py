767f27453aa458fd5687e487de8c78020bc86574
Trinh Pham <trinhpham@snaglobal.net>
2021-02-03 18:28:39 +0700

    finished GANs

--- codes/run.py
+++ codes/run.py
@@ -56,11 +56,11 @@ def parse_args(args=None):
     parser.add_argument('--max_steps', default=100000, type=int)
     parser.add_argument('--warm_up_steps', default=None, type=int)
 
-    parser.add_argument('--save_checkpoint_steps', default=1000, type=int)
-    parser.add_argument('--valid_steps', default=1000, type=int)
-    parser.add_argument('--log_steps', default=100, type=int,
+    parser.add_argument('--save_checkpoint_steps', default=100, type=int)
+    parser.add_argument('--valid_steps', default=50, type=int)
+    parser.add_argument('--log_steps', default=50, type=int,
                         help='train log every xx steps')
-    parser.add_argument('--test_log_steps', default=1000,
+    parser.add_argument('--test_log_steps', default=50,
                         type=int, help='valid/test log every xx steps')
 
     parser.add_argument('--nentity', type=int, default=0,
